14652b96576e3274fae0e67b272c3d863c0a9410
Denis <denispetrasso77@gmail.com>
2025-09-08 20:47:52 -0300

    Ajuste ORden de Pago

--- CapaVista/FrmGestionOrdenCompra.Designer.cs
+++ CapaVista/FrmGestionOrdenCompra.Designer.cs
@@ -45,6 +45,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -56,19 +57,18 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lbltituloHome = new System.Windows.Forms.Label();
             this.pictureBox8 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbltituloHome = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -78,7 +78,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 90);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(763, 381);
+            this.tabControl1.Size = new System.Drawing.Size(933, 381);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -94,7 +94,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(755, 355);
+            this.tabPage1.Size = new System.Drawing.Size(925, 355);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Nueva Orden de Compra";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -103,7 +103,7 @@
             // 
             this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCancelar.Enabled = false;
-            this.btnCancelar.Location = new System.Drawing.Point(624, 266);
+            this.btnCancelar.Location = new System.Drawing.Point(794, 266);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(120, 35);
             this.btnCancelar.TabIndex = 7;
@@ -115,7 +115,7 @@
             // 
             this.btnEnviarOrdenes.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnEnviarOrdenes.Enabled = false;
-            this.btnEnviarOrdenes.Location = new System.Drawing.Point(472, 266);
+            this.btnEnviarOrdenes.Location = new System.Drawing.Point(654, 266);
             this.btnEnviarOrdenes.Name = "btnEnviarOrdenes";
             this.btnEnviarOrdenes.Size = new System.Drawing.Size(120, 35);
             this.btnEnviarOrdenes.TabIndex = 6;
@@ -126,7 +126,7 @@
             // btnModificar
             // 
             this.btnModificar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnModificar.Location = new System.Drawing.Point(101, 266);
+            this.btnModificar.Location = new System.Drawing.Point(310, 266);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(120, 35);
             this.btnModificar.TabIndex = 5;
@@ -138,7 +138,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(383, 13);
+            this.label2.Location = new System.Drawing.Point(573, 28);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(240, 22);
             this.label2.TabIndex = 4;
@@ -154,9 +154,9 @@
             this.Producto,
             this.Cotizacion,
             this.Enviar});
-            this.dataGridView2.Location = new System.Drawing.Point(257, 90);
+            this.dataGridView2.Location = new System.Drawing.Point(494, 90);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(487, 150);
+            this.dataGridView2.Size = new System.Drawing.Size(420, 150);
             this.dataGridView2.TabIndex = 3;
             // 
             // IdProducto
@@ -198,7 +198,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(211, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(420, 150);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
@@ -221,7 +221,7 @@
             this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveBorder;
             this.textBox1.Location = new System.Drawing.Point(10, 64);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "Buscador...";
             // 
@@ -229,7 +229,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 13);
+            this.label1.Location = new System.Drawing.Point(103, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(240, 22);
             this.label1.TabIndex = 0;
@@ -246,16 +246,28 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(755, 355);
+            this.tabPage2.Size = new System.Drawing.Size(925, 355);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Gestion de Ordenes de Compra";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::CapaVista.Properties.Resources.SIGNO_REFRESCAR;
+            this.pictureBox1.Location = new System.Drawing.Point(152, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(485, 24);
+            this.label3.Location = new System.Drawing.Point(650, 24);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(80, 22);
             this.label3.TabIndex = 12;
@@ -269,10 +281,10 @@
             this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn2,
             this.dataGridViewComboBoxColumn1});
-            this.dataGridView3.Location = new System.Drawing.Point(345, 97);
+            this.dataGridView3.Location = new System.Drawing.Point(499, 97);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.ReadOnly = true;
-            this.dataGridView3.Size = new System.Drawing.Size(401, 150);
+            this.dataGridView3.Size = new System.Drawing.Size(420, 150);
             this.dataGridView3.TabIndex = 11;
             // 
             // dataGridViewTextBoxColumn2
@@ -303,7 +315,7 @@
             this.dataGridView4.Name = "dataGridView4";
             this.dataGridView4.ReadOnly = true;
             this.dataGridView4.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView4.Size = new System.Drawing.Size(307, 150);
+            this.dataGridView4.Size = new System.Drawing.Size(420, 150);
             this.dataGridView4.TabIndex = 10;
             this.dataGridView4.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView4_CellClick);
             // 
@@ -341,7 +353,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(71, 24);
+            this.label4.Location = new System.Drawing.Point(148, 24);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(130, 22);
             this.label4.TabIndex = 8;
@@ -355,47 +367,35 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(769, 72);
+            this.panel1.Size = new System.Drawing.Size(937, 72);
             this.panel1.TabIndex = 27;
-            // 
-            // lbltituloHome
-            // 
-            this.lbltituloHome.AutoSize = true;
-            this.lbltituloHome.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbltituloHome.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lbltituloHome.Location = new System.Drawing.Point(269, 24);
-            this.lbltituloHome.Name = "lbltituloHome";
-            this.lbltituloHome.Size = new System.Drawing.Size(287, 37);
-            this.lbltituloHome.TabIndex = 6;
-            this.lbltituloHome.Text = "Orden de Compra";
             // 
             // pictureBox8
             // 
             this.pictureBox8.Image = global::CapaVista.Properties.Resources.pedido1;
-            this.pictureBox8.Location = new System.Drawing.Point(213, 11);
+            this.pictureBox8.Location = new System.Drawing.Point(297, 11);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(50, 50);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox8.TabIndex = 32;
             this.pictureBox8.TabStop = false;
             // 
-            // pictureBox1
+            // lbltituloHome
             // 
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::CapaVista.Properties.Resources.SIGNO_REFRESCAR;
-            this.pictureBox1.Location = new System.Drawing.Point(152, 71);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(30, 20);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 13;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.lbltituloHome.AutoSize = true;
+            this.lbltituloHome.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltituloHome.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbltituloHome.Location = new System.Drawing.Point(353, 24);
+            this.lbltituloHome.Name = "lbltituloHome";
+            this.lbltituloHome.Size = new System.Drawing.Size(287, 37);
+            this.lbltituloHome.TabIndex = 6;
+            this.lbltituloHome.Text = "Orden de Compra";
             // 
             // FrmGestionOrdenCompra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(769, 483);
+            this.ClientSize = new System.Drawing.Size(937, 483);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.tabControl1);
             this.Name = "FrmGestionOrdenCompra";
@@ -410,12 +410,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
